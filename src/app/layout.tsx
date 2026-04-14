ec0434ffd58bc5814453775ddebed0e93d157ec5
Cristian Simion <cristisimion84@yahoo.co.uk>
2026-04-14 07:34:43 -0700

    feat: add app layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <main style={{ padding: 20 }}>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
