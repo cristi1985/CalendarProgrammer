da10db5e6cc0cc235ca57fc0460307f38ffe4aaf
cristisimion84 <cristisimion84@yahoo.co.uk>
2026-04-14 09:19:35 -0700

    Fix in callback url so user is no longer redirected to login page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -8,6 +8,9 @@ export default function SignInPage() {
 
     await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options:{
+        redirectTo: `${window.location.origin}/auth/callback`
+      }
     })
   }
 
