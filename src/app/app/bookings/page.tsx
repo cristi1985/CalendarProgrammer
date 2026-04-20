0fa49a690cea60c39a02615a5a0356f968102ad9
cristisimion84 <cristisimion84@yahoo.co.uk>
2026-04-20 12:37:06 -0700

    Remove dead code

--- src/app/app/bookings/page.tsx
+++ src/app/app/bookings/page.tsx
@@ -4,18 +4,6 @@ import { redirect } from 'next/navigation'
 import { createBooking } from './actions'
 import { cancelBooking, updateBooking } from './manage-actions'
 
-function toDateTimeLocalValue(date: Date) {
-  const pad = (value: number) => value.toString().padStart(2, '0')
-
-  const year = date.getFullYear()
-  const month = pad(date.getMonth() + 1)
-  const day = pad(date.getDate())
-  const hours = pad(date.getHours())
-  const minutes = pad(date.getMinutes())
-
-  return `${year}-${month}-${day}T${hours}:${minutes}`
-}
-
 export default async function BookingsPage() {
   const result = await syncAuthenticatedUser()
 
