8855684a3730bf60a861e3312b629caf730ffd09
Cristian Simion <cristisimion84@yahoo.co.uk>
2026-05-07 08:43:28 +0300

    test: enable React component tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import path from 'path'
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: 'jsdom',
     globals: true,
-    include: ['src/**/*.test.ts'],
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
   },
   resolve: {
     alias: {
